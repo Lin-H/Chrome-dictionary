114d11b9f87603157df8d4c1cd94f7db85d61b03
Lin-H <lh04667649@qq.com>
2016-05-16 01:12:11 +0800

    add pronunciation

--- script/content-script.js
+++ script/content-script.js
@@ -1,21 +1,37 @@
 var selection = window.getSelection();
-$('body').append('<div id="_dictionary"></div>');
-var dialog = $('#_dictionary');
+$('body').append('<div id="_dictionary"></div><audio id="_audio"></audio>');
+var dialog = $('#_dictionary'),
+    audio = $('#_audio'),
+    content;
+
+dialog.on('click', function(e) {
+    return false;
+});
 
 document.onkeyup = function(event) {
     if (event.code !== 'ControlLeft' && event.code !== 'ControlRight') {
         return true;
     }
-    chrome.extension.sendMessage({word: selection.toString().trim()}, function(response) {
-        var content = '英[' + response.content.ph_en + ']  美[' + response.content.ph_am + ']<br/>';
-        content += response.content.word_mean.join('<br/>');
-        dialog.html(content);
-        document.addEventListener('selectionchange', hideDialog);
-        dialog.show();
-    });
+    chrome.extension.sendMessage({word: selection.toString().trim()}, iciba);
 };
+
+function iciba(response) {
+    content = '<p>英[' + response.content.ph_en + ']<span class="_icon" data-audio="' + response.content.ph_en_mp3.replace(/(http:)|(https:)/, '') + '"></span>';
+    content += '美[' + response.content.ph_am + ']<span class="_icon" data-audio="' + response.content.ph_am_mp3.replace(/(http:)|(https:)/, '') + '"></span></p>';
+    content += response.content.word_mean.join('<br/>');
+
+    dialog.html(content);
+    $('span._icon').on('click', function(e) {
+        audio[0].src = e.currentTarget.dataset.audio;
+        audio[0].play();
+        return false;
+    });
+
+    document.addEventListener('click', hideDialog);
+    dialog.show();
+}
 
 function hideDialog(event) {
     dialog.hide();
-    document.removeEventListener('selectionchange', hideDialog);
+    document.removeEventListener('click', hideDialog);
 }
--- script/script.js
+++ script/script.js
@@ -1,6 +1,16 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    iciba(request, sender, sendResponse);
+    return true;
+});
+
+function iciba(request, sender, sendResponse) {
     $.getJSON('http://fy.iciba.com/ajax.php?a=fy&w=' + request.word, function(data) {
         sendResponse(data);
     });
-    return true;
-});
+}
+
+function merriam(request, sender, sendResponse) {
+    $.get('http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + request.word + '?key=APIKEY', function(data) {
+        sendResponse(data);
+    });
+}

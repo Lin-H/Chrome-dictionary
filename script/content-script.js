461d5457e11643aa8124d38e200c3779ef72d9ef
Lin-H <lh04667649@qq.com>
2016-05-17 23:23:08 +0800

    play audio in the background page

--- script/content-script.js
+++ script/content-script.js
@@ -1,7 +1,6 @@
-var selection = window.getSelection();
-$('body').append('<div id="_dictionary"></div><audio id="_audio"></audio>');
-var dialog = $('#_dictionary'),
-    audio = $('#_audio'),
+$('body').append('<div id="_dictionary"></div>');
+var selection = window.getSelection(),
+    dialog = $('#_dictionary'),
     content;
 
 dialog.on('click', function(e) {
@@ -12,21 +11,25 @@ document.onkeyup = function(event) {
     if (event.code !== 'ControlLeft' && event.code !== 'ControlRight') {
         return true;
     }
-    chrome.extension.sendMessage({word: selection.toString().trim()}, iciba);
+    chrome.extension.sendMessage({
+        type: 'iciba',
+        word: selection.toString().trim()
+    }, iciba);
 };
 
 function iciba(response) {
-    content = '<p>英[' + response.content.ph_en + ']<span class="_icon" data-audio="' + response.content.ph_en_mp3.replace(/(http:)|(https:)/, '') + '"></span>';
-    content += '美[' + response.content.ph_am + ']<span class="_icon" data-audio="' + response.content.ph_am_mp3.replace(/(http:)|(https:)/, '') + '"></span></p>';
+    content = '<p>英[' + response.content.ph_en + ']<span class="_icon" data-audio="' + response.content.ph_en_mp3 + '"></span>';
+    content += '美[' + response.content.ph_am + ']<span class="_icon" data-audio="' + response.content.ph_am_mp3 + '"></span></p>';
     content += response.content.word_mean.join('<br/>');
-
     dialog.html(content);
+
     $('span._icon').on('click', function(e) {
-        audio[0].src = e.currentTarget.dataset.audio;
-        audio[0].play();
+        chrome.extension.sendMessage({
+            type: 'playsound',
+            audioSrc: e.currentTarget.dataset.audio
+        });
         return false;
     });
-
     document.addEventListener('click', hideDialog);
     dialog.show();
 }
--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
+$('body').append('<audio></audio>');
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    iciba(request, sender, sendResponse);
+    switch (request.type) {
+        case 'playsound':
+            playSound(request);
+            return false;
+        case 'iciba':
+            iciba(request, sender, sendResponse); return true;
+        default:
+            return false;
+    }
     return true;
 });
 
@@ -13,4 +23,10 @@ function merriam(request, sender, sendResponse) {
     $.get('http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + request.word + '?key=APIKEY', function(data) {
         sendResponse(data);
     });
+}
+
+function playSound(request) {
+    var audio = $('audio')[0];
+    audio.src = request.audioSrc;
+    audio.play();
 }
